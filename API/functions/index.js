5cd1dbf440c9f60a01b63af2611496890cc77a9b
Manuel Soares <mc.soares@campus.fct.unl.pt>
2022-06-07 21:06:13 +0100

    API bug fixing

--- API/functions/index.js
+++ API/functions/index.js
@@ -7,20 +7,21 @@
 const functions = require('firebase-functions');
 // [END functionsimport]
 const admin = require("firebase-admin");
-var express = require("express"),
-  router = express.Router();
+//var express = require("express");
+  //router = express.Router();
 admin.initializeApp()
 
 // Moments library to format dates.
-const moment = require('moment');
+//const moment = require('moment');
 // CORS Express middleware to enable CORS Requests.
-const cors = require('cors')({
+/* const cors = require('cors')({
   origin: true,
-});
+}); */
 
 const db = admin.firestore();
-const { v4: uuidv4 } = require("uuid");
-const { body, param, validationResult } = require("express-validator");
+//const { v4: uuidv4 } = require("uuid");
+//const { body, param, validationResult } = require("express-validator");
+//const { snapshotConstructor } = require('firebase-functions/v1/firestore');
 
 // [START all]
 /**
@@ -40,46 +41,61 @@ const { body, param, validationResult } = require("express-validator");
  * This endpoint supports CORS.
  */
 
+//accelerometer > 0.4
 exports.POSTInfo = functions.https.onRequest(async (req, res) => {
   if(req.method!=='POST'){
     res.status(403).send('Forbidden!');
     return;
   }
-  const errors = validationResult(req);
+  /* const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).send({ errors: errors.array() });
-  }
+  } */
   try{
     const ts = req.body.timestamp;
     const press = req.body.pressure;
     const light = req.body.light;
-    const gyro = req.body.gyroscope;
-    if(ts==null || press==null || light==null || gyro==null){
+    const accX = req.body.accelerometerX;
+    const accY = req.body.accelerometerY;
+    const accZ = req.body.accelerometerZ;
+    if(ts==null || press==null || light==null || accX==null || accY==null || accZ==null){
       res.status(400).send('Bad Request! Parameter missing.');
     }
-    let snapShot= await db
-    .collection('/sleepInfo')
-    .where("ts", "==", ts)
-    .get()
-    console.log(snapShot)
-    if (snapShot != null || snapShot.exists) {
-      //console.log("h")
-      res.status(409).send('There is already information for that timestamp');
+    let hour = ts.split(" ")[1].split(":")[0];
+    let minutes = ts.split(" ")[1].split(":")[1];
+    let seconds = ts.split(" ")[1].split(":")[2];
+    if(Number(hour)<0 || Number(hour)>23 || Number(minutes)<0 || Number(minutes)>59 || Number(seconds)<0 || Number(seconds)>59.99){
+      res.status(409).send("You cannot send invalid time stamps.");
     }
-    
-    let date = ts.split(" ")[0].split("-")[2];
-    console.log(date + " - day")
-    await db.collection("/sleepInfo").doc(ts).create({
-      ts: ts,
-      press: press,
-      light: light,
-      gyro: gyro,
-      day: Number(date),
-    });
+    const snapShot= db
+    .collection('/sleepInfo')
+    .doc(ts);
+    /* .where("ts", "==", ts)
+    .get() */
 
-    res.status(200).send('done, time:' + ts.toString());
-  } catch (error) {
-    return res.status(500).send(error);
+    await snapShot.get().then((doc) => {
+      if (doc.exists) {
+          console.log("exists already");
+          res.status(409).send('There is already information for that timestamp');
+      }
+    });
+    
+      let date = ts.split(" ")[0].split("-")[2];
+      //console.log(date + " - day")
+      await db.collection("/sleepInfo").doc(ts).create({
+        ts: ts,
+        press: press,
+        light: light,
+        accX: Number(accX),
+        accY: Number(accY),
+        accZ: Number(accZ),
+        day: Number(date),
+      });
+
+      res.status(200).send('done, time:' + ts.toString());
+    //}
+  } catch (err) {
+    return res.status(500).send(err);
   }
   
 })
@@ -89,14 +105,15 @@ exports.GETSleep = functions.https.onRequest(async (req, res) => {
     res.status(403).send('Forbidden!');
     return;
   }
-  const errors = validationResult(req);
+  /* const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).send({ errors: errors.array() });
-  }
+  } */
   try{
-    const mode = req.body.mode;
-    if(mode==null){
-      res.status(400).send('Bad Request! Parameter missing.');
+    const mode = req.query.mode;
+    const num = Number(mode);
+    if(mode==null || !Number.isInteger(num) || num < 0){
+      return res.status(400).send('Bad Request! Parameter missing.');
     }
     var sleepInterval = [];
     let prevSleep=false;
@@ -104,12 +121,39 @@ exports.GETSleep = functions.https.onRequest(async (req, res) => {
     var today = new Date().getDate();
     var priorDays = new Date();
     priorDays.setDate(today - mode);
+    let prevAccx=0;
+    let prevAccy=0;
+    let prevAccz=0;
+    let firstRound=true;
     
-    db.collection("/sleepInfo").where("day", ">=", priorDays.getDate()).get().then( (querySnapshot) => {
+    db.collection("/sleepInfo").where("day", ">=", today-mode).get().then( (querySnapshot) => {
       querySnapshot.forEach((doc)=>{
         
-        let isSleeping = doc.data().light<100 && doc.data().press>10 && doc.data().gyro<2;
-        
+        let accDiffx=0;
+        let accDiffy=0;
+        let accDiffz=0;
+
+        let isSleeping;
+
+        if(firstRound){
+          prevAccx=doc.data().accX;
+          prevAccy=doc.data().accY;
+          prevAccz=doc.data().accZ;
+          isSleeping = doc.data().light<100 && doc.data().press>10;
+          firstRound=false;
+        }
+        else {
+          accDiffx=doc.data().accX-prevAccx;
+          accDiffy=doc.data().accY-prevAccy;
+          accDiffz=doc.data().accZ-prevAccz;
+          isSleeping = doc.data().light<100 && doc.data().press>10 && (accDiffx<10 && accDiffx>-10 && accDiffy<10 && accDiffx>-10 && accDiffz<10 && accDiffz>-10);
+          //console.log("is it sleeping? "+isSleeping)
+        }
+        prevAccx=doc.data().accX;
+        prevAccy=doc.data().accY;
+        prevAccz=doc.data().accZ;
+
+        //console.log("acc diff", accDiffx)
         // doc.data() is never undefined for query doc snapshots
         //console.log(doc.id, " => ", doc.data());
         //console.log(doc.id + ' - isSleeping '+ isSleeping)
@@ -135,12 +179,17 @@ exports.GETSleep = functions.https.onRequest(async (req, res) => {
           }
         }
       })
+      if (prevSleep){
+        const obj=[auxInterval[0], auxInterval[auxInterval.length-1]];
+            sleepInterval.push(obj);
+            auxInterval=[];
+      }
       return res.status(200).send(sleepInterval);
     });
     
-  } catch (error) {
-    console.log(error);
-    return res.status(500).send(error);
+  } catch (err) {
+    //console.log(error);
+    return res.status(500).send(err);
   }
 })
 
@@ -159,8 +208,6 @@ exports.GETSleep = functions.https.onRequest(async (req, res) => {
       
       (async () => {
         try {
-
-          await admin.auth().verifyIdToken(req.header('AuthToken'))
 
           try {
 
